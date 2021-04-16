0e34517902020bdedad98959c455d250b6e0e44e
Seiryu-alt <seiryusain@gmail.com>
2021-04-16 05:28:22 +0000

    [add] ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  root to: 'home#top'
   get 'homes/about'
+  resources :users, only: [:index, :show]
+  resources :books
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
