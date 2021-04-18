1b8196957dfde5ed52ca19d53638a7468b0154dd
Seiryu-alt <seiryusain@gmail.com>
2021-04-18 06:51:09 +0000

    [add] ユーザー情報編集ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @new_book = Book.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
