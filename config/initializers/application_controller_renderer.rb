bd2f6c2592c92ecf5fbb2b84c8f9257039dd76ac
Seiryu-alt <seiryusain@gmail.com>
2021-04-18 07:08:04 +0000

    [add] userのupdate機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fdd68aee34722403665e3c39da24873afd955cfb1e1ef49673fd49a5148a4e7a54492f754574c204d7263ef44a8c9472a6bf77c7288228475c613475a2ffd37d'
